07f5a6b379a5bfc291c5c08ff30d511632792f47
jonjon98 <yapzh.jon@gmail.com>
2023-03-12 01:56:08 +0800

    feat: fixes mashalling and unmarshalling codes from java and python side.

--- frontend/dfs/marshalling.py
+++ frontend/dfs/marshalling.py
@@ -6,38 +6,54 @@ def marshal(unmarshalled_data):
   # marshal logic
   marshalled_data = bytearray()
   for key, value in unmarshalled_data.items():
-      key_len = len(key.encode('utf-8'))
-      marshalled_data += key_len.to_bytes(3, byteorder='big', signed=False)
+      key_len = str(len(key.encode('utf-8')))
+      if len(key_len) == 1:
+        padded_key_len = "00" + key_len
+      elif len(key_len) == 2:
+        padded_key_len = "0" + key_len
+      elif len(key_len) == 3:
+        padded_key_len = key_len
+      marshalled_data += padded_key_len.encode('utf-8')
       marshalled_data += key.encode('utf-8')
-      value_len = len(value.encode('utf-8'))
-      marshalled_data += value_len.to_bytes(3, byteorder='big', signed=False)
+      
+      value_len = str(len(value.encode('utf-8')))
+      if len(value_len) == 1:
+        padded_value_len = "00" + value_len
+      elif len(value_len) == 2:
+        padded_value_len = "0" + value_len
+      elif len(value_len) == 3:
+        padded_value_len = value_len
+      marshalled_data += padded_value_len.encode('utf-8')
       marshalled_data += value.encode('utf-8')
   return marshalled_data
 
 def unmarshal(marshalled_data):
   # unmarshal logic
   unmarshalled_data = {}
+  decoded_marshalled_data = str(marshalled_data.decode('utf-8'))
+  even_decoded_marshalled_data = decoded_marshalled_data[1::2]
+
   idx = 0
   flag = "key"
 
   key = ""
   value = ""
-  while idx != len(marshalled_data):
+  while idx != len(even_decoded_marshalled_data):
     if flag == "key":
-      length = int(marshalled_data[idx] + marshalled_data[idx + 1] + marshalled_data[idx + 2])
+      length = int(even_decoded_marshalled_data[idx]) * 100 + int(even_decoded_marshalled_data[idx + 1]) * 10 + int(even_decoded_marshalled_data[idx + 2])
       idx += 3
 
-      key = marshalled_data[idx:idx + length]
+      key = even_decoded_marshalled_data[idx:idx + length]
       idx += length
       flag = "value"
 
     elif flag == "value":
-      length = int(marshalled_data[idx] + marshalled_data[idx + 1] + marshalled_data[idx + 2])
+      length = int(even_decoded_marshalled_data[idx]) * 100 + int(even_decoded_marshalled_data[idx + 1]) * 10 + int(even_decoded_marshalled_data[idx + 2])
       idx += 3
 
-      value = marshalled_data[idx:idx + length]
+      value = even_decoded_marshalled_data[idx:idx + length]
       idx += length
-      unmarshalled_data[key.decode('utf-8')] = value.decode('utf-8')
+      unmarshalled_data[key] = value
       key = ""
       value = ""
       flag = "key"
